6cf2d8aea6f8635665ddbb4e8ce5ab9cb324033b
Jack Dickinson <jackdickinson090@hotmail.com>
2024-07-29 23:23:39 +0200

    Updates

--- App.js
+++ App.js
@@ -13,7 +13,9 @@ db.once("open", () => {
 })
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
@@ -29,10 +31,21 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`)
+})
+
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
 })
 
 app.listen(3000, () => {
