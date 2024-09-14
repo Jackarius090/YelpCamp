15f3f6fda8d9e913ce3a994399e534348aabeafe
Jack Dickinson <jackdickinson090@hotmail.com>
2024-09-14 07:20:08 +0200

    update

--- models/campground.js
+++ models/campground.js
@@ -15,7 +15,7 @@ const CampgroundSchema = new Schema({
         }
     ]
 });
-
+ 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
         await Review.deleteMany({
