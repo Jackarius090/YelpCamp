d7cbfb0ea80d58aa5257d6c46eb8e6693c349dd1
Jack Dickinson <jackdickinson090@hotmail.com>
2024-09-22 22:15:27 +0200

    code refactoring

---
+++ controllers/users.js
@@ -0,0 +1,44 @@
+const User = require('../models/user');
+
+module.exports.renderRegister = (req, res) => {
+    res.render('users/register');
+}
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}
+
+module.exports.renderLogin = (req, res) => {
+    res.render('users/login');
+}
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+module.exports.login = (req, res) => {
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
+    res.redirect(redirectUrl);
+};
+
+module.exports.logout = (req, res) => {
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+}
